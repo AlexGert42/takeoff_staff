dae7dbc7ead0ca6e94830b19025cdc93b0201186
AlexGert42 <alex.gert1812@gmail.com>
2022-04-12 21:02:05 +0300

    fix

--- src/context/index.ts
+++ src/context/index.ts
@@ -1,5 +1,3 @@
-export { store } from '@context/store'
-
 import { authUser, loginUser, logout, registerUser } from '@context/redusers/authReducer'
 import { ActiveOwerlay } from '@context/redusers/appReducer'
 import { getContacts, removeContact, editContact, setContact, searchContact, removeContacts } from '@context/redusers/contactsReducer'
